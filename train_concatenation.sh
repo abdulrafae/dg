b7885ad77f13181b9f00e2de7d7604bb75ddf359
abdulrafae <rafae015@gmail.com>
2021-04-11 06:58:12 -0400

    Update train_concatenation.sh

--- train_concatenation.sh
+++ train_concatenation.sh
@@ -4,22 +4,22 @@ FAIRSEQROOT=fairseq
 
 GPU=0
 MAINSRC=$1
-PHONETICSRC=$2
+ALGO=$2
 TRG=$3
-echo "Train "$MAINSRC$PHONETICSRC-$TRG
+echo "Train "$MAINSRC$ALGO-$TRG
 
 BASE_PATH=$DATA_ROOT
-TEXT=$BASE_PATH/$MAINSRC$PHONETICSRC\_$TRG/
+TEXT=$BASE_PATH/$MAINSRC$ALGO\_$TRG/
 
-DATA_BIN=$BASE_PATH/data_bin/$MAINSRC$PHONETICSRC\_$TRG}/concat/
-LOG_DIR=$BASE_PATH/log/$MAINSRC$PHONETICSRC\_$TRG/concat/
-CPKT_DIR=$BASE_PATH/checkpoint/$MAINSRC$PHONETICSRC\_$TRG/concat/
-RESULT_DIR=$BASE_PATH/result/$MAINSRC$PHONETICSRC\_$TRG/concat/
+DATA_BIN=$BASE_PATH/data_bin/$MAINSRC$ALGO\_$TRG}/concat/
+LOG_DIR=$BASE_PATH/log/$MAINSRC$ALGO\_$TRG/concat/
+CPKT_DIR=$BASE_PATH/checkpoint/$MAINSRC$ALGO\_$TRG/concat/
+RESULT_DIR=$BASE_PATH/result/$MAINSRC$ALGO\_$TRG/concat/
 mkdir -p $CPKT_DIR $DATA_BIN $LOG_DIR $RESULT_DIR
 
-echo "Preprocess "$MAINSRC$PHONETICSRC-$TRG
+echo "Preprocess "$MAINSRC$ALGO-$TRG
 python $FAIRSEQROOT/preprocess.py \
---source-lang $MAINSRC$PHONETICSRC \
+--source-lang $MAINSRC$ALGO \
 --target-lang $TRG \
 --trainpref $TEXT/train.bpe \
 --validpref $TEXT/valid.bpe \
@@ -27,7 +27,7 @@ python $FAIRSEQROOT/preprocess.py \
 --worker 10 | tee $LOG_DIR/prepro.out
 
 # Train the Concatenation model
-echo "Train "$MAINSRC$PHONETICSRC-$TRG
+echo "Train "$MAINSRC$ALGO-$TRG
 CUDA_VISIBLE_DEVICES=$GPU python $FAIRSEQROOT/train.py $DATA_BIN \
 --lr 0.25 \
 --clip-norm 0.1 \
@@ -40,14 +40,14 @@ CUDA_VISIBLE_DEVICES=$GPU python $FAIRSEQROOT/train.py $DATA_BIN \
 --save-dir $CPKT_DIR | tee $LOG_DIR/train.out
 
 #Translate using Concatenated test data
-echo "Translate  "$MAINSRC$PHONETICSRC-$TRG
+echo "Translate  "$MAINSRC$ALGO-$TRG
 CUDA_VISIBLE_DEVICES=$GPU python $FAIRSEQROOT/interactive.py $DATA_BIN \
 --path $CPKT_DIR/checkpoint_best.pt \
 --batch-size 32 \
 --beam 12 \
 --buffer-size 64 \
 --remove-bpe \
---input $TEXT/test.bpe.$MAINSRC$PHONETICSRC | tee $RESULT_DIR/gen.sys
+--input $TEXT/test.bpe.$MAINSRC$ALGO | tee $RESULT_DIR/gen.sys
 
 #Evaluate BLEU for translated data
 grep ^H $RESULT_DIR/gen.sys | cut -f3- > $RESULT_DIR/hypo.tok.$TRG
