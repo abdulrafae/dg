6a946d1ce6b9c277e239cf8b43674fd57072ac7d
abdulrafae <rafae015@gmail.com>
2021-10-22 10:52:27 -0400

    Update encode_data.py

--- encode_data.py
+++ encode_data.py
@@ -6,10 +6,10 @@ from shutil import copyfile as cp
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--source', '-s', type=str, help='Source language')
 parser.add_argument('--target', '-t', type=str, help='Target language')
-parser.add_argument('--mappingfile', '-m', type=str, help='Mapping file')
+parser.add_argument('--groupingfile', '-m', type=str, help='Mapping file')
 parser.add_argument('--inpath', '-i',  type=int, help='Input path')
 parser.add_argument('--filenames', '-f', type=str, default='train,valid,test', help='Input filenames comma separated')
-parser.add_argument('--output', '-o', type=str, help='Output path')
+parser.add_argument('--outpath', '-o', type=str, help='Output path')
 parser.add_argument('--algo', '-a', type=str, default='algo1', help='Algorithm format for file extension')
 args = parser.parse_args()
 
@@ -17,13 +17,13 @@ src = args.source
 tgt = args.target
 inpath = args.infile
 filenames = args.filenames.split(',')
-mappingfile = args.mappingfile
+groupingfile = args.groupingfile
 inpath = args.inpath
 outpath = args.outpath
 algo = args.algo
 
 groups = dict()
-with codecs.open(mappingfile,'r',encoding='utf-8') as f:
+with codecs.open(groupingfile,'r',encoding='utf-8') as f:
 	for line in f:
 		parts = line.strip().split(' : ')
 		word = parts[0]
