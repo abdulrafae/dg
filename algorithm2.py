20a4de98bb3bc3e602fb6c8a55648e3a53b15155
abdulrafae <rafae015@gmail.com>
2021-11-04 16:23:54 -0400

    Update algorithm2.py

--- algorithm2.py
+++ algorithm2.py
@@ -69,91 +69,20 @@ def fit_function(k, lamb):
 	return poisson.pmf(k, lamb)
 parameters, cov_matrix = curve_fit(fit_function, bin_middles, entries)
 
-tmp_vocab = copy.deepcopy(vocab_list)
-n = len(vocab_list)
-K = int(0.2*n)
+id = 0
+while len(tmp)!=0:
+	size = np.random.poisson(lam=parameters[0])
+	while size==0:	
+		size = np.random.poisson(lam=parameters[0])
+	for i in range(size):
+		word = random.choice(tmp)
+		random_mapping[word] = id
+		tmp.remove(word)
+	lengths.remove(size)
+	id += 1
 
-K = len(encoded_dict.keys())
-
-metaphone_dict = defaultdict(list)
-for word in vocab.keys():
-	encoded_word = jellyfish.metaphone(word)
-	if encoded_word == '':
-		metaphone_dict[word].append(word)
-	else:
-		metaphone_dict[encoded_word].append(word)
-
-n = len(vocab_list)
-percentage = 0.2
-out_clusters = int(percentage*n)
-max_len = 0
-max_key = ""
-for key in metaphone_dict.keys():
-	if max_len < len(metaphone_dict[key]):
-		max_len = len(metaphone_dict[key])
-		max_key = key
-
-K = max_len
-print("Max Metaphone size = "+str(K))
-
-uniform_cluster_count = int(n/K)
-uniform_clusters = defaultdict(list)
-for i in range(n):
-	idx = random.randint(1,uniform_cluster_count)
-	uniform_clusters[idx].append(vocab_list[i])
-
-print("Start Diverse Clusters!")
-new_meta_dict = copy.deepcopy(metaphone_dict)
-metaphone_cluster_count = len(metaphone_dict.keys())
-tmp_vocab = copy.deepcopy(vocab_list)
-final_clusters = defaultdict(list)
-key_i = 0
-
-current_count = n
-for key in metaphone_dict.keys():
-	if current_count == 0:
-		break
-	cluster = metaphone_dict[key]
-	cluster_len = len(metaphone_dict[key])
-	if cluster_len == K:
-		cluster_list = []
-		for key2 in new_meta_dict.keys():
-			new_cluster = new_meta_dict[key2]
-			if len(new_cluster) > 0:
-				idx = random.randint(len(new_cluster))
-				word = new_cluster[idx]
-				cluster_list.append(word)
-				del new_cluster[idx]
-				new_meta_dict[key2] = new_cluster				
-		final_clusters[str(key_i)] = cluster_list
-		key_i += 1
-		print("Adding "+str(len(cluster_list))+" elements in cluster "+str(key_i))
-	elif cluster_len==0:
-		exit()
-	else:
-		j = cluster_len
-		sample_count = int(((j/float(K)))*len(list(new_meta_dict.keys())))
-		if sample_count == 0:
-			exit()
-		selected_keys = random.sample(new_meta_dict.keys(),sample_count)
-		cluster_list = []
-		for key2 in selected_keys:
-			new_cluster = new_meta_dict[key]
-			if len(new_cluster) > 0:
-				idx = random.randint(len(new_cluster))
-				word = new_cluster[idx]
-				cluster_list.append(new_cluster[idx])
-				del new_cluster[idx]
-				new_meta_dict[key2] = new_cluster
-		final_clusters[str(key_i)] = cluster_list
-		key_i += 1
-		print("Adding "+str(len(cluster_list))+" elements in cluster "+str(key_i))
-	current_count -= len(cluster_list)
-
-print("Writing Groups!")
-out_file = codecs.open(outpath+"/algo2_grouping.txt",'w',encoding='utf-8')
-for key in final_clusters.keys():
-	for word in final_clusters[key]:
-		out_file.write(word+" : "+str(key)+"\n")
-out_file.close()
+target = codecs.open(outpath+'/algo2_grouping.txt','w',encoding='utf-8')
+for key in random_mapping.keys():
+	target.write(str(key)+" : "+str(random_mapping[key])+"\n")
+target.close()
 print("DONE!")
